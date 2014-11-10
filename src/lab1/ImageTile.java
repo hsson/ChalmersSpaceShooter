8b64a1f5b6b7c7f4130de3ea2bbf507452b74726
Alexander Håkansson <al3xand3r.h@gmail.com>
2014-11-10 11:01:51 +0100

    Implements a generic image tile which can be used by all the game entities

--- src/lab1/ImageTile.java
+++ src/lab1/ImageTile.java
@@ -1,13 +1,14 @@
 package lab1;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class ImageTile extends GameTile {
 
     private final Image tileImage;
+    private Position tilePosition;
 
-    public ImageTile(Image tileImage){
+    public ImageTile(Position tilePosition, Image tileImage){
+        this.tilePosition = tilePosition;
         this.tileImage = tileImage;
     }
 
@@ -15,5 +16,13 @@ public class ImageTile extends GameTile {
     public void draw(final Graphics g, final int x, final int y, Dimension d) {
         Graphics2D g2 = (Graphics2D) g;
         g2.drawImage(this.tileImage, x, y, d.width, d.height, null);
+    }
+
+    public Position getTilePosition() {
+        return this.tilePosition;
+    }
+
+    public void setTilePosition(Position newPosition) {
+        this.tilePosition = newPosition;
     }
 }
