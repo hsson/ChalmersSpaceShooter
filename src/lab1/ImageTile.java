98e4f48803960cc31f3e49316e9b6963f500dc5a
Alexander Håkansson <al3xand3r.h@gmail.com>
2014-11-10 11:21:59 +0100

    Removes position from the ImageTile to separate logic from view

--- src/lab1/ImageTile.java
+++ src/lab1/ImageTile.java
@@ -5,10 +5,8 @@ import java.awt.*;
 public class ImageTile extends GameTile {
 
     private final Image tileImage;
-    private Position tilePosition;
 
-    public ImageTile(Position tilePosition, Image tileImage){
-        this.tilePosition = tilePosition;
+    public ImageTile(Image tileImage){
         this.tileImage = tileImage;
     }
 
@@ -16,13 +14,5 @@ public class ImageTile extends GameTile {
     public void draw(final Graphics g, final int x, final int y, Dimension d) {
         Graphics2D g2 = (Graphics2D) g;
         g2.drawImage(this.tileImage, x, y, d.width, d.height, null);
-    }
-
-    public Position getTilePosition() {
-        return this.tilePosition;
-    }
-
-    public void setTilePosition(Position newPosition) {
-        this.tilePosition = newPosition;
     }
 }
