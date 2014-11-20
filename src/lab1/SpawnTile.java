7bd193922a12918dfc869cecd3772b645fafc49f
Alexander Håkansson <al3xand3r.h@gmail.com>
2014-11-20 14:31:03 +0100

    Implements a check so powerups and monsters can't spawn on top of each other

--- src/lab1/SpawnTile.java
+++ src/lab1/SpawnTile.java
@@ -1,11 +1,16 @@
 package lab1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpawnTile {
 
     private MonsterSpawn monsterSpawn;
     private PowerUpSpawn powerUpSpawn;
 
     private int width, yPos;
+
+    private static List<Integer> occupiedXPositions = new ArrayList<Integer>();
 
     public SpawnTile(int width, int yPos) {
         this.width = width;
@@ -18,5 +23,19 @@ public class SpawnTile {
     public void spawn() {
         monsterSpawn.spawnMonster();
         powerUpSpawn.spawnPowerUp();
+
+        occupiedXPositions.clear();
+    }
+
+    public static boolean isXPosOccupied(int xPos) {
+        if (occupiedXPositions.contains(xPos)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void addOccupiedXPos(int xPos) {
+        occupiedXPositions.add(xPos);
     }
 }
